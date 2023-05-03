2e6a7c51e6479d5c1ff01909134fa6539e725ab6
wiamaiz <ouiame.faiz@gmail.com>
2023-05-03 16:05:57 -0700

    added codes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,36 +1,36 @@
+#include "lists.h"
+
 /**
- * delete_nodeint_at_index - deletes the node at index index
- * of a listint_t linked list
- * @head: pointer to a pointer to the head of the list
- * @index: index of the node to be deleted
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
  *
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: 1 (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev;
-    unsigned int i;
+    if (*head == NULL) {
+        return -1; // if the list is empty, return -1 indicating failure
+    }
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+    if (index == 0) {
+        listint_t *temp = *head;
+        *head = (*head)->next;
+        free(temp);
+        return 1; // if the first node needs to be deleted, update the head pointer and return 1 indicating success
+    }
 
-    current = *head;
-    prev = NULL;
-    for (i = 0; i < index && current != NULL; i++)
-    {
-        prev = current;
+    listint_t *current = *head;
+    for (unsigned int i = 0; i < index - 1; i++) {
+        if (current->next == NULL) {
+            return -1; // if the end of the list is reached before the target node is found, return -1 indicating failure
+        }
         current = current->next;
     }
-    if (i < index || current == NULL)
-        return (-1);
 
-    if (prev != NULL)
-        prev->next = current->next;
-    else
-        *head = current->next;
-
-    free(current);
-
-    return (1);
+    listint_t *temp = current->next;
+    current->next = temp->next;
+    free(temp);
+    return 1; // delete the target node, update the links, and return 1 indicating success
 }
 
