1b4d110639fd44c68e5dc885768f4cbf7f4ab631
wiamaiz <ouiame.faiz@gmail.com>
2023-05-03 16:10:46 -0700

    added codes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,36 +1,30 @@
-#include "lists.h"
-
-/**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
- *
- * Return: 1 (Success), or -1 (Fail)
- */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    if (*head == NULL) {
-        return -1; // if the list is empty, return -1 indicating failure
+    listint_t *prev_node, *current_node;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current_node = *head;
+
+    if (index == 0)
+    {
+        *head = current_node->next;
+        free(current_node);
+        return (1);
     }
 
-    if (index == 0) {
-        listint_t *temp = *head;
-        *head = (*head)->next;
-        free(temp);
-        return 1; // if the first node needs to be deleted, update the head pointer and return 1 indicating success
+    for (i = 0; i < index; i++)
+    {
+        prev_node = current_node;
+        current_node = current_node->next;
+        if (current_node == NULL)
+            return (-1);
     }
 
-    listint_t *current = *head;
-    for (unsigned int i = 0; i < index - 1; i++) {
-        if (current->next == NULL) {
-            return -1; // if the end of the list is reached before the target node is found, return -1 indicating failure
-        }
-        current = current->next;
-    }
-
-    listint_t *temp = current->next;
-    current->next = temp->next;
-    free(temp);
-    return 1; // delete the target node, update the links, and return 1 indicating success
+    prev_node->next = current_node->next;
+    free(current_node);
+    return (1);
 }
 
