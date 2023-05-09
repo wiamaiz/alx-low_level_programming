3192701883933f0e44b9cae0120f35b1e3f56a64
wiamaiz <ouiame.faiz@gmail.com>
2023-05-08 18:44:29 -0700

    added codes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,53 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * read_textfile - reads a text file and prints its contents to POSIX stdout
  * @filename: the name of the file to read
- * @letters: the number of letters to read and print
+ * @letters: the maximum number of letters to read from the file
  *
- * Return: the actual number of letters read and printed, or 0 on error
+ * Return: the actual number of letters read and printed, or 0 on failure
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, r, w;
-	char *buf;
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    buffer = malloc(sizeof(char) * (letters + 1));
+    if (buffer == NULL)
+        return (0);
 
-	buf = malloc(sizeof(char) * (letters + 1));
-	if (buf == NULL)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buffer);
+        return (0);
+    }
 
-	r = read(fd, buf, letters);
-	if (r == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-	buf[r] = '\0';
+    buffer[bytes_read] = '\0';
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1 || w != r)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
-	free(buf);
-	close(fd);
-
-	return (r);
+    free(buffer);
+    close(fd);
+    return (bytes_written);
 }
 
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,17 +1,96 @@
-def remove_duplicates(lst1, lst2):
-    """Returns a new list that contains only the unique elements from lst1 and lst2"""
-    unique_elements = []
-    for item in lst1:
-        if item not in unique_elements:
-            unique_elements.append(item)
-    for item in lst2:
-        if item not in unique_elements:
-            unique_elements.append(item)
-    return unique_elements
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
-# Example usage
-list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-result = remove_duplicates(list1, list2)
-print(result)
+#define BUFFER_SIZE 128
+
+void print_error(char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void print_elf_header(Elf64_Ehdr *hdr) {
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", hdr->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n",
+           hdr->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
+           hdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
+    printf("  Data:                              %s\n",
+           hdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+           hdr->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+    printf("  Version:                           %d (current)\n", hdr->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            ");
+    switch (hdr->e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("UNIX - Linux\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("UNIX - NetBSD\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("  ABI Version:                       %d\n", hdr->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              ");
+    switch (hdr->e_type) {
+        case ET_NONE:
+            printf("NONE (Unknown type)\n");
+            break;
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
+            break;
+        case ET_EXEC:
+            printf("EXEC (Executable file)\n");
+            break;
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
+            break;
+        case ET_CORE:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("  Entry point address:               0x%lx\n", hdr->e_entry);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        print_error("Usage: elf_header elf_filename");
+    }
+
+    char *filename = argv[1];
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        print_error("Error opening file");
+    }
+
+    Elf64_Ehdr hdr;
+    ssize_t n = read(fd, &hdr, sizeof(hdr));
+    if (n != sizeof(hdr)) {
+        print_error("Error reading ELF header");
+    }
+
+    if (hdr.e_ident[EI_MAG0] != ELFMAG0 ||
+        hdr.e_ident[EI_MAG1] != ELFMAG1 ||
+        hdr.e_ident[EI_MAG2] != ELFMAG2 ||
+        hdr.e_ident[EI_MAG3] != ELFMAG3) {
+        print_error("Invalid ELF magic number");
+    }
+
+    print_elf_header(&hdr);
+
+    close(fd);
+    exit(EXIT_SUCCESS);
 
